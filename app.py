d9358c541c0d54058f63abfbf6482f3c3160b89b
kelvin-lu <kelvinlu@mit.edu>
2019-04-23 00:16:39 -0400

    test for seeing if push causes stuff to happen

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, request, session, g, redirect, url_for, abort, \
+     render_template, flash, redirect
+
+app = Flask(__name__) # create the application instance :)
+
+@app.route('/', methods=['POST'])
+def on_webhook(payload):
+    print(payload)
+
+if __name__ == '__main__':
+    app.run(debug=True)
