f1491fa89c6493199e012e47930ae08dc81ccf49
kelvin-lu <kelvinlu@mit.edu>
2019-04-29 21:38:47 -0400

    can identify redundant features

--- index.js
+++ index.js
@@ -1,28 +1,42 @@
 // Checks API example
 // See: https://developer.github.com/v3/checks/ to learn more
-const fs = require('fs')
-const git = require('isomorphic-git')
-git.plugins.set('fs', fs)
+const fs = require('fs');
+const git = require('isomorphic-git');
+const travis = require('./travis.js');
+git.plugins.set('fs', fs);
 
 /**
  * This is the main entrypoint to your Probot app
  * @param {import('probot').Application} app
  */
 module.exports = app => {
-  app.on(['check_suite.completed'], check);
+  app.on(['check_run.completed'], check);
+}
 
-  async function check (context) {
-    const startTime = new Date()
-
-    // Do stuff
-    const s = context.payload.check_suite;
-    // Probot API note: context.repo() => {username: 'hiimbex', repo: 'testing-things'}
-    console.info(s);
+async function check (context) {
+  const checkRun = context.payload.check_run;
+  if (checkRun.check_suite.head_branch !== 'master') {
+    return;
   }
+  const detailsUrl = checkRun.details_url;
+  const redundantFeatures = travis.getTravisRedundantFeatures(detailsUrl);
+  const gitDir = await downloadRepo(checkRun.check_suite.repository.html_url);
+  const 
+}
 
-  app.on('push', print)
+async function downloadRepo(url, ref) {
+  const tempDir = tmp.dirSync();
+  await git.clone({
+      dir: tempDir.name,
+      url,
+      ref: ref | 'master',
+      singleBranch: true,
+      depth: 10,
+  })
+  return tempDir;
+}
 
-  async function print (context) {
-    console.info(context)
-  }
+async function findRedundantFeaturesFromContext(checkRun) {
+  const buildUrl = checkRun.details_url;
+  
 }
