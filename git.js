e9ecff1eb09aa617f66784ae1a092a112536c170
kelvin-lu <kelvinlu@mit.edu>
2019-05-01 17:41:12 -0400

    one commit per prune - tree

--- git.js
+++ git.js
@@ -3,7 +3,7 @@ const git = require('isomorphic-git');
 const tmp = require('tmp');
 git.plugins.set('fs', fs);
 
-const PRUNE_MESSAGE = 'Pruning Redundant Feature: $feature';
+const PRUNE_MESSAGE = 'Pruning Redundant Feature:';
 const BALLET_AUTHOR = {
     name: 'Ballet',
     email: 'dai-lab@mit.edu' // Some email...
@@ -12,10 +12,9 @@ const BALLET_AUTHOR = {
 const removeRedundantFeatures = async (context, features) => {
     const repoUrl = context.payload.repository.html_url;
     const repoDir = await downloadRepo(repoUrl);
-    for(let i = 0; i < features.length; i++) {
-        const feature = features[i];
-        await removeFeatureFromRepo(context, repoDir.name, feature);
-    }
+    const featureFiles = files.map(feature => feature + '.py');
+    const removalTree = createFileTree(featureFiles)
+    await removeFilesFromRepo(context, repoDir.name, removalTree);
 
     repoDir.removeCallback();
 }
@@ -32,25 +31,65 @@ const downloadRepo = async (url, ref) => {
     return tempDir;
 }
 
-const removeFeatureFromRepo = async (context, dir, feature) => {
-    const featurePath = feature + '.py';
-    const pathPieces = featurePath.split('/');
+const createFileTree = (files) => {
+    /**
+     * Creates a tree-like structure where each prefix 
+     * is a part of the path. E.g. the file:
+     *    features/contrib/user_kelvin/feature_adder.py
+     * becomes the trie:
+     *    features -> contrib -> user_kelvin -> feature_adder.py
+     * 
+     * @params files a list of POSIX-style file paths
+     * @returns a head node representing the top-level directory
+     */
+    const head = {path: "", next: {}};
+    files
+        .forEach(feature => {
+        feature_pieces = feature.split('/');
+        recursivelyCreateFileNodes(head, feature_pieces);
+    })
+    return head;
+}
+
+const recursivelyCreateFileNodes = (node, path) => {
+    if (path.length === 0) {
+        return;
+    }
+    const next_piece = path[0];
+    if (!node.next[next_piece]) {
+        node.next[next_piece] = {
+            path: next_piece,
+            next: {},
+            end: path.length === 1,
+        };
+    }
+    return recursivelyCreateFileNodes(node.next[next_piece], path.slice(1));
+}
+
+const removeFilesFromRepo = async (context, dir, removalTree) => {
     const headRef = await git.resolveRef({dir, ref: 'HEAD'});
     const refObj = await git.readObject({dir, oid: headRef});
-    let headTree = refObj.object.tree;
-    headTree = await recursivelyCreateTrees(
+    const oldHead = refObj.object.tree;
+    const newHead = await recursivelyCreateTrees(
         context,
         dir,
-        pathPieces,
-        headTree);
+        removalTree,
+        oldHead);
+    
+    // If nothing changes, do nothing
+    if (oldHead === newHead) {
+        return;
+    }
 
     const commitInfo = {
         parents: [headRef],
-        tree: headTree.data.sha,
+        tree: newHead,
         message: PRUNE_MESSAGE.replace('$feature', prettyPrintFeature(feature)),
         author: BALLET_AUTHOR,
     }
+    // Create a commit on github
     const {data: commit} = await context.github.gitdata.createCommit(context.repo(commitInfo));
+    // Make the commit the head of the master repo
     await context.github.gitdata.updateRef(context.repo({
         ref: 'heads/master',
         sha: commit.sha,
@@ -59,30 +98,31 @@ const removeFeatureFromRepo = async (context, dir, feature) => {
     return await git.pull({dir, ref: 'master'});
 }
 
-const recursivelyCreateTrees = async (context, dir, path, tree) => {
-    const treeObj = await git.readObject({dir, oid: tree});
+const recursivelyCreateTrees = async (context, dir, pathTree, treeSha) => {
+    const treeObj = await git.readObject({dir, oid: treeSha});
     let objects = [];
-    for(let i = 0; i < treeObj.object.entries.length; i++) {
-        const obj = treeObj.object.entries[i];
-        if (obj.type === 'blob'
-            && path[0] === obj.path
-            && path.length === 1) {
-            continue;
-        } else if (obj.type === 'tree'
-            && path[0] === obj.path
-            && path.length > 1) {
-            const newTree = await recursivelyCreateTrees(
-                context,
-                dir,
-                path.slice(1),
-                obj.oid
-            );
-            if (newTree) {
-                objects.push({
-                    ...obj,
-                    sha: newTree.data.sha,
-                })
+    let treeHasChanged = false;
+    for(let obj in  treeObj.object.entries) {
+        if (pathTree.next[obj.oid]) {
+            treeHasChanged = true;
+            // If there's a next piece of the path, this is a tree.
+            // Continue recursing and creating new trees
+            if (pathTree.next[obj.oid].next) {
+                const newTreeSha = await recursivelyCreateTrees(
+                    context,
+                    dir,
+                    path.slice(1),
+                    obj.oid
+                );
+                if (newTree) {
+                    objects.push({
+                        ...obj,
+                        sha: newTreeSha,
+                    })
+                }
             }
+            // else, do nothing 
+            // logically removes this file from the tree
         } else {
             objects.push({
                 ...obj,
@@ -90,13 +130,22 @@ const recursivelyCreateTrees = async (context, dir, path, tree) => {
             })
         }
     }
-    return await context.github.gitdata.createTree(context.repo({tree: objects}));
+
+    // If the tree has changed, send it to github.
+    // else, just return the old hash
+    if (treeHasChanged) {
+        const newTree =  await context.github.gitdata.createTree(context.repo({tree: objects}));
+        return newTree.data.sha;
+    } else {
+        return treeSha;
+    }
 }
 
-const prettyPrintFeature = feature => {
-    const parts = feature.split('/');
-    const { length } = parts;
-    return parts[length - 2] + ', ' + parts[length - 1];
+const prettyPrintFeature = file => {
+    const parts = file.split('/');
+    const user = parts[parts.length - 2].split('_')[1];
+    const feature = parts[parts.length - 1].split('_')[1];
+    return user + '.' + feature;
 }
 
 module.exports = { removeRedundantFeatures };
