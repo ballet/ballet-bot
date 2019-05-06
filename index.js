aeb24ef5d295a67b52d41590e95de418295aec06
kelvin-lu <kelvinlu@mit.edu>
2019-05-06 03:14:57 -0400

    back to a working state!

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Checks API example
 // See: https://developer.github.com/v3/checks/ to learn more
 const travis = require('./lib/travis.js');
-const git = require('./lib/git.js');
+const github = require('./lib/github.js');
 const prune = require('./lib/pruning.js');
 /**
  * This is the main entrypoint to your Probot app
@@ -12,8 +12,8 @@ module.exports = app => {
     const repoUrl = context.payload.repository.html_url;
     const detailsUrl = context.payload.check_run.details_url;
 
-    const repoDir = git.downloadRepo(repoUrl);
-    const config = git.getConfigFromRepo(repoDir.name);
+    const repoDir = await github.downloadRepo(repoUrl);
+    const config = await github.getConfigFromRepo(repoDir.name, context);
     const travisBuild = travis.getBuildIdFromDetailsUrl(detailsUrl);
 
     if (await shouldPruneRedundantFeatures(context, config, travisBuild)) {
@@ -25,11 +25,11 @@ module.exports = app => {
 };
 
 const shouldPruneRedundantFeatures = async (context, config, buildId) => {
-  if (!git.isOnMasterAfterMerge(context)) {
+  if (!github.isOnMasterAfterMerge(context)) {
     return false;
   } else if (!travis.doesBuildNotFailAllChecks(buildId)) {
     return false;
-  } else if (config.github.prune_action === 'no_action') {
+  } else if (config.github.pruning_action === 'no_action') {
     return false;
   }
 
